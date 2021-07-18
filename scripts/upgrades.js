86684d0da039244879e1f169e58842c9f2a666a7
Zachary Kline <zkline888@gmail.com>
2021-07-18 17:21:49 -0400

    upgrades ready to be input

--- scripts/upgrades.js
+++ scripts/upgrades.js
@@ -6,13 +6,14 @@
 // wool = 15
 
 class Upgrade {
-    constructor(upName, target, req1, req2, req3, req4, bonus, code) {
+    constructor(upName, target, req1, req2, req3, req4, req5, bonus, code) {
         this.upName = upName
         this.target = target
         this.req1 = req1
         this.req2 = req2
         this.req3 = req3
         this.req4 = req4
+        this.req5 = req5
         this.bonus = bonus
         this.code = code
     }
@@ -25,53 +26,56 @@ class Upgrade {
         upgradeCard.innerHTML = this.upName
         buyButton.innerHTML = `Buy Upgrade`
         buyButton.addEventListener('click', this.buyUpgrade)
-        buyButton.id = `${target}${code}`
+        buyButton.id = `${this.target.name}${this.code}`
+        upgradeCard.append(buyButton)
         upgradeSpot.append(upgradeCard)
     }
     checkUnlock() {
-        if (woolCount >= req1) {
+        if (this.target.numOfA >= this.req5) {
+            console.log('hello')
             this.createUpgrade()
             return true
         }
     }
-    canBuy() {
-
-    }
-    buyUpgrade() {
+    buyUpgrade = () => {
+        console.log(this)
         if (this.checkRequirements()) {
-            let getButton = document.getElementById(`#${target}${code}`)
+            let getButton = document.getElementById(`${this.target.name}${this.code}`)
             woolCount -= this.req1
             this.target.organs -= this.req2
             this.target.hide -= this.req3
             this.target.essence -= this.req4
             woolCounter.innerHTML = `Wool Count: ${commaIncluded(woolCount)}`
-            document.getElementById(`${animal.name}Organs`).innerHTML = `Organs: ${animal.organs}`
-            document.getElementById(`${animal.name}Hides`).innerHTML = `Hides: ${animal.hide}`
-            document.getElementById(`${animal.name}essences`).innerHTML = `Essences: ${animal.essence}`
-            this.target.wool = this.target.wool + (this.target.wool * this.bonus)
+            document.getElementById(`${this.target.name}Organs`).innerHTML = `Organs: ${this.target.organs}`
+            document.getElementById(`${this.target.name}Hides`).innerHTML = `Hides: ${this.target.hide}`
+            document.getElementById(`${this.target.name}essences`).innerHTML = `Essences: ${this.target.essence}`
+            this.target.wool = this.target.wool * this.bonus
             getButton.classList.add('used')
             getButton.removeEventListener('click', this.buyUpgrade)
         }
     }
     checkRequirements() {
-        
+        if (this.checkWool() && this.checkReq2() && this.checkReq3() && this.checkReq4()) {
+            return true
+        }
+        return false
     }
     checkWool() {
-        return this.req1 >= woolCount ? true : false
+        return this.req1 <= woolCount ? true : false
     }
     checkReq2() {
-        return this.req2 >= this.target.organs ? true : false
+        return this.req2 <= this.target.organs ? true : false
     }
     checkReq3() {
-        return this.req3 >= this.target.hide ? true : false
+        return this.req3 <= this.target.hide ? true : false
     }
     checkReq4() {
-        return this.req4 >= this.target.essence ? true : false
+        return this.req4 <= this.target.essence ? true : false
     }
 }
 
-let sheepUp1 = new Upgrade('Copper Shears', sheep, 500, 4, 1, 0, .5, 001)
+let sheepUp1 = new Upgrade('Copper Fleece', sheep, 10, 4, 1, 0, 10, 2, 001)
 
 const checkForUpgrade1 = setInterval(() => {
-    if(sheepUp1) stop(checkForUpgrade1)
+    if(sheepUp1.checkUnlock()) stop(checkForUpgrade1)
 }, 5000)
